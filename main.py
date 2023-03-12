e7d0cdce0a1bcf014090ffbcebb20b9ce4bc6b68
s25008 <s25008@pjwstk.edu.pl>
2023-03-12 17:45:12 +0100

    gotowe zadanie

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 # This is a sample Python script.
 import webbrowser
-pageUrl = 'https://github.com'
+import requests
 
-webbrowser.open(pageUrl)
+date = 20110126
+pageUrl = 'www.github.com'
+url = "http://archive.org/wayback/available?url="+pageUrl+"&timestamp="+str(date)
+response = requests.get(url)
+d = response.json()
+oldPage = d["archived_snapshots"]["closest"]["url"]
+
+webbrowser.open(oldPage)
